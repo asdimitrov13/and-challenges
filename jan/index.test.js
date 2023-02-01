12d228e6bba1bddc0a3bc3276469060334f0d723
Aleksandar Dimitrov <aleksandar.dimitrov@superdry.com>
2023-02-01 16:29:14 +0000

    Add more tests

--- jan/index.test.js
+++ jan/index.test.js
@@ -1,7 +1,7 @@
 import { challenge, findFirstCommand } from "./index";
 
 describe("January challenge", () => {
-  describe("Command detector", () => {
+  describe("Command detector (find first command in text)", () => {
     it("single command", () => {
       const text = "ABC [CTRL+C] asd";
 
@@ -27,7 +27,7 @@ describe("January challenge", () => {
     });
   });
 
-  describe("Text transformer: ", () => {
+  describe("Text transformer (solution): ", () => {
     it("one copy one paste", () => {
       const result = challenge(
         "the first[CTRL+C] Coding Challenge was [CTRL+V] string manipulation task"
@@ -52,6 +52,76 @@ describe("January challenge", () => {
       );
       expect(result).toBe(
         "the first Coding Challenge was string manipulation task"
+      );
+    });
+  });
+
+  describe("real data", () => {
+    it("line 1", () => {
+      const result = challenge(
+        "the big red[CTRL+C] fox jumps over [CTRL+V] lazy dog."
+      );
+
+      expect(result).toBe("the big red fox jumps over the big red lazy dog.");
+    });
+
+    it("line 2", () => {
+      const result = challenge(
+        "[CTRL+V]the tall oak tree towers over the lush green meadow."
+      );
+
+      expect(result).toBe(
+        "the tall oak tree towers over the lush green meadow."
+      );
+    });
+
+    it("line 3", () => {
+      const result = challenge(
+        "the sun shines down[CTRL+C] on [CTRL+V][CTRL+C] the busy [CTRL+V]."
+      );
+
+      expect(result).toBe(
+        "the sun shines down on the sun shines down the busy the sun shines down on the sun shines down."
+      );
+    });
+
+    it("line 4", () => {
+      const result = challenge(
+        "[CTRL+V]the tall oak tree towers over the lush green meadow."
+      );
+
+      expect(result).toBe(
+        "the tall oak tree towers over the lush green meadow."
+      );
+    });
+
+    it("line 5", () => {
+      const result = challenge(
+        "a majestic lion[CTRL+C] searches for [CTRL+V] in the tall grass."
+      );
+
+      expect(result).toBe(
+        "a majestic lion searches for a majestic lion in the tall grass."
+      );
+    });
+
+    it("line 6", () => {
+      const result = challenge(
+        "the shimmering star[CTRL+X]Twinkling in the dark, [CTRL+V] shines bright."
+      );
+
+      expect(result).toBe(
+        "Twinkling in the dark, the shimmering star shines bright."
+      );
+    });
+
+    it("line 7", () => {
+      const result = challenge(
+        "[CTRL+X]a fluffy white cloud drifts [CTRL+V][CTRL+C] across the sky, [CTRL+V]"
+      );
+
+      expect(result).toBe(
+        "a fluffy white cloud drifts  across the sky, a fluffy white cloud drifts "
       );
     });
   });
